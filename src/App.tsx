6d5a62f47205b60dd6d151c8df8ba60580ad9d78
niso <takeseoeasy@gmail.com>
2022-01-20 15:44:23 +0700

    feat: create broken terms

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Terms from './components/Terms/Terms';
 
 function App() {
-    return (
-        <div className='App'>
-            <div className='container'>
-              <Header></Header>
-            </div>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Header></Header>
+        <Terms
+          title='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+          buttons={[{ title: 'Присоединиться', type: 'filled' }, { title: 'Присоединиться', type: 'filled' }]}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
