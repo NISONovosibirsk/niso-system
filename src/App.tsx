2adca495862f7f03c264b2b60ce73f119e981bf5
niso <takeseoeasy@gmail.com>
2022-01-20 13:28:15 +0700

    feat: create register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
 
 function App() {
     return (
         <div className='App'>
             <div className='container'>
               <Header></Header>
+              <Register />
             </div>
         </div>
     );
