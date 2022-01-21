5f70abb6ae1495b28604936022d3686d1ceea0f1
niso <takeseoeasy@gmail.com>
2022-01-21 13:26:36 +0700

    feat(blog): add BlogCards

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BlogCard from '../BlogCard/BlogCard';
 
 import './Blog.scss';
 
@@ -6,6 +7,22 @@ const Blog = () => {
   return (
     <section className='blog'>
       <h2 className='blog__title'>Информация для посетителей</h2>
+      <BlogCard
+        title='Sed ut perspiciatis'
+        paragraph='Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.'
+      />
+      <BlogCard
+        title='Sed ut perspiciatis'
+        paragraph='Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.'
+      />
+      <BlogCard
+        title='Sed ut perspiciatis'
+        paragraph='Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.'
+      />
+      <BlogCard
+        title='Sed ut perspiciatis'
+        paragraph='Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.'
+      />
     </section>
   );
 };
