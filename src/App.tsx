666182d408d87e86dc5fb80957c91b9a92b4a5c1
niso <takeseoeasy@gmail.com>
2022-01-21 13:07:29 +0700

    feat: create blog

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Terms from './components/Terms/Terms';
 import Register from './components/Register/Register';
+import Blog from './components/Blog/Blog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           title='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
           buttons={[{ title: 'Присоединиться', type: 'filled' }, { title: 'Присоединиться', type: 'filled' }]}
         />
+        <Blog />
       </div>
     </div>
   );
---
+++ src/components/Blog/Blog.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Blog.scss';
+
+const Blog = () => {
+  return (
+    <section className='blog'>
+      <h2 className='blog__title'>Информация для посетителей</h2>
+    </section>
+  );
+};
+
+export default Blog;
