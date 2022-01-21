50e77d6ed8d99fb2d059cec7fd4625dfb8c8a2f8
niso <takeseoeasy@gmail.com>
2022-01-21 13:20:34 +0700

    feat: create BlogCard

---
+++ src/components/BlogCard/BlogCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../Button/Button';
+import './BlogCard.scss';
+
+const BlogCard = ({
+  title,
+  paragraph,
+  path,
+}: {
+  title: string;
+  paragraph: string;
+  path?: string;
+}) => {
+  return (
+    <div className='blog-card'>
+      <h3 className='blog-card__title'>{title}</h3>
+      <p className='blog-card__paragraph'>{paragraph}</p>
+      <Button title='Читать' />
+    </div>
+  );
+};
+
+export default BlogCard;
