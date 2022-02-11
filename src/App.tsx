aa71bcf002e8b32b4e1aa3d7ec23293d688b6362
niso <takeseoeasy@gmail.com>
2022-02-11 14:54:49 +0700

    feat: showing and editing form to send added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { FormConstructor, Header, SavedForms } from './components';
+import { FormConstructor, Header, SavedForms, SavedForm } from './components';
+import { useTypeSelector } from './hooks/useTypeSelector';
 
 function App() {
+
+    const { currentForm } = useTypeSelector(state => state.form);
+
     return (
         <div className='App'>
             <div className='container'>
                 <Header />
                 <FormConstructor />
                 <SavedForms />
+                {currentForm.length && <SavedForm />}
+                
             </div>
         </div>
     );
