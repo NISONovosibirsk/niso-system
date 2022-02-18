3f4d7ba40a268c2d3e94a0ba56f01ff2eab02ee4
niso <takeseoeasy@gmail.com>
2022-02-18 09:49:34 +0700

    feat: modify excel export

--- src/components/ExportExcel/ExportExcel.tsx
+++ src/components/ExportExcel/ExportExcel.tsx
@@ -2,18 +2,18 @@ import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 import './ExportExcel.scss';
 
 const ExportExcel = ({ savedForm }) => {
-    const handleElementsType = element => {
-        switch (element.type) {
+    const handleElementsType = (formElement, i) => {
+        switch (formElement.type) {
             case 'header':
             case 'title':
             case 'subtitle':
                 return (
-                    <tr>
+                    <tr key={i}>
                         <th
                             style={{ background: '#0f0', fontSize: '24px' }}
                             colSpan={2}
                         >
-                            {element.placeholder}
+                            {formElement.placeholder}
                         </th>
                     </tr>
                 );
@@ -22,28 +22,83 @@ const ExportExcel = ({ savedForm }) => {
             case 'radio':
                 return null;
             default:
-                return (
-                    <>
-                        <tr>
-                            <td>{element.label}</td>
-                            {element.isRequired ? <td>*</td> : null}
-                        </tr>
-                        <tr>
+                const currentElemSplit = formElement.label.split(' / ');
+                const previousElemSplit = savedForm.content[i - 1].label
+                    ? savedForm.content[i - 1].label.split(' / ')[0]
+                    : null;
+
+                if (currentElemSplit[1]) {
+                    let j = i;
+                    const colsName: [string] = [currentElemSplit[1]];
+                    while (
+                        savedForm.content[j + 1] &&
+                        savedForm.content[j].label.split(' / ')[0] ===
+                            savedForm.content[j + 1].label.split(' / ')[0]
+                    ) {
+                        colsName.push(
+                            savedForm.content[j + 1].label.split(' / ')[1]
+                        );
+                        j++;
+                    }
+
+                    if (currentElemSplit[0] === previousElemSplit) {
+                        return;
+                    } else {
+                        return (
+                            <>
+                                <tr>
+                                    <td></td>
+                                    {colsName.map((colName, i) => (
+                                        <td key={i}>{colName}</td>
+                                    ))}
+                                </tr>
+                                <tr
+                                    key={i}
+                                    style={
+                                        i % 2
+                                            ? {}
+                                            : {
+                                                  background:
+                                                      'rgba(216, 228, 188)',
+                                              }
+                                    }
+                                >
+                                    <td>{formElement.label.split(' / ')[0]}</td>
+                                    <td>{''}</td>
+                                </tr>
+                            </>
+                        );
+                    }
+                } else {
+                    return (
+                        <tr
+                            key={i}
+                            style={
+                                i % 2
+                                    ? {}
+                                    : { background: 'rgba(216, 228, 188)' }
+                            }
+                        >
+                            <td>{formElement.label}</td>
                             <td>{''}</td>
                         </tr>
-                    </>
-                );
+                    );
+                }
         }
     };
 
     return (
         <>
-            <table className='export-excel' id='table'>
-                {savedForm.content.map(element => handleElementsType(element))}
+            <table className='export-excel' id={savedForm._id}>
+                <tbody>
+                    {savedForm.content.map((formElement, i) =>
+                        handleElementsType(formElement, i)
+                    )}
+                </tbody>
             </table>
             <ReactHTMLTableToExcel
                 className='export-excel__button'
-                table='table'
+                table={savedForm._id.toString()}
                 buttonText=''
                 filename={savedForm.subtitle}
                 sheet={savedForm.subtitle}
