f1e652455991d59a5d0766f73934e89d9560db14
niso <takeseoeasy@gmail.com>
2022-03-10 16:18:47 +0700

    ref

---
+++ src/components/main/User/pages/Constructor/Constructor.tsx
@@ -0,0 +1,7 @@
+import './Constructor.scss';
+
+const Constructor = () => {
+    return <section className='user-constructor'></section>;
+};
+
+export default Constructor;
--- src/components/main/User/pages/Profile/Profile.tsx
+++ src/components/main/User/pages/Profile/Profile.tsx
@@ -2,26 +2,26 @@ import './Profile.scss';
 
 const Profile = () => {
     return (
-        <div className='profile'>
-            <div className='profile-slider'>
-                <div className='profile-slider__item'>
+        <div className='user-profile'>
+            <div className='user-profile-slider'>
+                <div className='user-profile-slider__item'>
                     <p>Основное</p>
                     <hr />
                 </div>
-                <div className='profile-slider__item'>
+                <div className='user-profile-slider__item'>
                     <p>Оповещения</p>
                     <hr />
                 </div>
-                <div className='profile-slider__item'>
+                <div className='user-profile-slider__item'>
                     <p>Документы</p>
                     <hr />
                 </div>
             </div>
 
-            <form className='profile-documents'>
-                <div className='profile-documents-form'>
-                    <p className='profile-documents-form__header'>Код образовательного учреждения:</p>
-                    <input className='profile-documents-form__input' placeholder='Впешите данные документов'/>
+            <form className='user-profile-documents'>
+                <div className='user-profile-documents-form'>
+                    <p className='user-profile-documents-form__header'>Код образовательного учреждения:</p>
+                    <input className='user-profile-documents-form__input' placeholder='Впешите данные документов'/>
                 </div>
 
             </form>
