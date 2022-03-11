fe37d88532e281cbb64feb09931aaa81c788f464
niso <takeseoeasy@gmail.com>
2022-03-11 13:44:25 +0700

    route

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ import {
     ChangePassword,
     StatusPopup,
     User,
-    FormConstructor,
 } from './components/main';
-import { Home, Profile } from './components/main/User/pages';
+import { Constructor, Home, Profile } from './components/main/User/pages';
 
 function App() {
     const dispatch = useDispatch();
@@ -60,8 +59,7 @@ function App() {
                 <Route path='user' element={<User />}>
                     <Route path='home' element={<Home />} />
                     <Route path='profile/*' element={<Profile />} />
-                    <Route path='constructor' element={<FormConstructor />} />
-
+                    <Route path='constructor' element={<Constructor />} />
                 </Route>
             </Routes>
             <StatusPopup />
