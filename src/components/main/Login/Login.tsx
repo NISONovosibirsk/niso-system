69a9c63ee040be299670928302c36444a5deac2d
NISONovosibirsk <97866158+NISONovosibirsk@users.noreply.github.com>
2022-08-26 11:37:08 +0700

    Update Login.tsx

--- src/components/main/Login/Login.tsx
+++ src/components/main/Login/Login.tsx
@@ -51,7 +51,7 @@ const Login = () => {
 
     return (
         <section className='login'>
-            <NisoLogo className='login__logo' />
+            <MaisLogo className='login__logo' />
             <AuthForm
                 onSubmit={() => {}}
                 isValid={isValid}
